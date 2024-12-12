f314a0acf2b94b6e0ce2f93441729ce68e84799f
xchbx <ceina.ellison@gmail.com>
2024-12-12 10:39:20 -0500

    edited detail method

--- app.py
+++ app.py
@@ -58,16 +58,10 @@ def create():
 @app.route('/plant/<plant_id>')
 def detail(plant_id):
     """Display the plant detail page & process data from the harvest form."""
-
-    # TODO: Replace the following line with a database call to retrieve *one*
-    # plant from the database, whose id matches the id passed in via the URL.
-    plant_to_show = ''
-
-    # TODO: Use the `find` database operation to find all harvests for the
-    # plant's id.
-    # HINT: This query should be on the `harvests` collection, not the `plants`
-    # collection.
-    harvests = ''
+    # Fetch the plant with the given 'plant_id' from the 'plants' collection
+    plant_to_show = mongo.db.plants.find_one({'_id': ObjectId(plant_id)})
+    # Fetch all harvests associated with the plant's id from the harvest's collection
+    harvests = mongo.db.harvests.find_one(plant_id)
 
     context = {
         'plant' : plant_to_show,
